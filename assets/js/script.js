89a52c424be322b2bc21e7bb751def674d1a95b7
rehanaf <rehanalfaqih287@gmail.com>
2023-12-30 06:39:25 +0700

    update

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// On page load or when changing themes, best to add inline in `head` to avoid FOUC
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  document.documentElement.classList.add('dark')
+} else {
+  document.documentElement.classList.remove('dark')
+}
+
+let gantiTema = document.getElementById('ganti-tema')
+gantiTema.addEventListener('click', (e) => {
+  document.getElementById('light-icon').classList.toggle('hidden')
+  document.getElementById('dark-icon').classList.toggle('hidden')
+  document.documentElement.classList.toggle('dark')
+  localStorage.theme = (localStorage.theme == 'light') ? 'dark' : 'light'
+})
