826c89450d630a0837a5710421767f407d5bff5d
rehanaf <rehanalfaqih287@gmail.com>
2023-12-30 07:43:09 +0700

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
 if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
   document.documentElement.classList.add('dark')
-  document.getElementById('light-icon').classList.toggle('hidden')
-  document.getElementById('dark-icon').classList.toggle('hidden')
+  document.getElementById('light-icon').classList.remove('hidden')
+  document.getElementById('dark-icon').classList.add('hidden')
 } else {
   document.documentElement.classList.remove('dark')
+  document.getElementById('light-icon').classList.add('hidden')
+  document.getElementById('dark-icon').classList.remove('hidden')
 }
 
 document.getElementById('ganti-tema').addEventListener('click', (e) => {
